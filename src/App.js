1a9a6251159b8e3bb8609e93a67dc6d7dc47094c
INS140 <IsaacNSaettel@gmail.com>
2023-03-07 02:14:29 -0800

    counter complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <></>
-  );
+  const [count, setCount] = useState(0)
+
+  const handleCountIncrease = () => {
+    count !== '' && count < 998
+      ? setCount(count + 1)
+      : setCount(1)
+  }
+
+  const handleCountReset = () => setCount(0)
+
+  const handleCountDecrease = () => setCount(count - 1)
+
+  const handleCountManual = (e) => {
+    Number(e.target.value)
+      ? setCount(Number(e.target.value))
+      : e.target.value === '' 
+      ? setCount('')
+      : setCount(0)
+  }
+
+  return <>
+      <div className='count-container flex-row'>
+        <input
+          type='text'
+          value={count}
+          onChange={handleCountManual}
+          maxLength='3'
+        />
+      </div>
+      <div className='btn-container flex-row'>
+        <button
+        className='sub'
+          onClick={handleCountDecrease}
+          disabled={count === 0 || count === ''}
+        >-</button>
+        <button onClick={handleCountReset}>RESET</button>
+        <button
+          className='add'
+          onClick={handleCountIncrease}
+        >+</button>
+      </div>
+    </>
 }
 
 export default App;
